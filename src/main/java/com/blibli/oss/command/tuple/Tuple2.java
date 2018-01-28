ad785acfd50751abdba77f40dccd6a96655a0ca8
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2018-01-28 22:03:29 +0700

    change data using lombok

--- src/main/java/com/blibli/oss/command/tuple/Tuple2.java
+++ src/main/java/com/blibli/oss/command/tuple/Tuple2.java
@@ -1,24 +1,20 @@
 package com.blibli.oss.command.tuple;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Eko Kurniawan Khannedy
  */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tuple2<FIRST, SECOND> {
 
   private FIRST first;
 
   private SECOND second;
-
-  public Tuple2(FIRST first, SECOND second) {
-    this.first = first;
-    this.second = second;
-  }
-
-  public FIRST getFirst() {
-    return first;
-  }
-
-  public SECOND getSecond() {
-    return second;
-  }
 }
